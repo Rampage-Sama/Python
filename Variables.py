f31b0beb8873d25c5a89505ecf35fee0b44dfcac
Ofentse Rampa <ofentse.rampa7@gmail.com>
2021-02-11 16:01:37 +0200

    Initial commit

---
+++ Variables.py
@@ -0,0 +1,9 @@
+#defining variables!
+character_fcar = "McLaren"
+character_console = "PlayStation"
+character_team = "Real Madrid"
+my_num = "24"
+print("My favourite car model is a "  + character_fcar + ",")
+print("My favourite console is a " + character_console + "," )
+print("My favourite football team is " + character_team + "." )
+print("I am currently " + my_num + " years old." ) 
